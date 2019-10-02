706e58587faed48aa9f511d993874a02dc575969
cheraivan <52885107+cheraivan@users.noreply.github.com>
2019-10-02 14:05:38 +0300

    AddService

---
+++ KopterBot/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KopterBot.Services
+{
+    class UserService
+    {
+    }
+    class AdminService { }
+    class DronService { }
+    class BuisnessTaskService { }
+    class StepService { }
+    class HubService { }
+    class StorageService { }
+
+    class ServiceProvider
+    {
+
+    }
+}
